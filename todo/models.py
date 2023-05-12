e599ce043ee97d8a78774a65a5e0dc4d972c2729
iamavhishek <abhishek.bca76@hsm.edu.np>
2023-05-12 12:58:40 +0545

    Removed id from model

--- todo/models.py
+++ todo/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 # Create your models here.
 class Todolists(models.Model):
-    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=250)
     description = models.CharField(max_length=5000)
     status = models.CharField(default='Awaiting', max_length=15)
